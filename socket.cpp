ed5439b638c46a2cd9b7c08c06de2d88f83abc5e
lnwin <405824853@qq.com>
2022-04-12 11:28:39 +0800

    通信协议测试通过

--- socket.cpp
+++ socket.cpp
@@ -16,14 +16,32 @@ void socket::receive()
     {
         ba.resize(uSocket->pendingDatagramSize());
         uSocket->readDatagram(ba.data(), ba.size());
-
+        bool ok;
+        int MESG_NUMBER=ba.mid(2,1).toHex().toInt(&ok, 16)+ba.mid(3,1).toHex().toInt(&ok, 16)*256;
+        int MESG_NUMBER_nCRC=MESG_NUMBER-4;
         qDebug() << ba.toHex()+"\n";
-        qDebug() << ba.remove(7,27).toHex()+"\n";
-        uint16_t data =ba.toUShort();
+        qDebug()<<MESG_NUMBER;
+        uint8_t MESG_head[7];
 
-        uint8_t sk[4]= {4,"c",4,9};
+        for(int i=0;i<7;i++)
+        {
+            MESG_head[i]=(uint8_t)ba.at(i);
+        }
 
-        qDebug() << QString::number(SOC_CRC->FastCRC16(data,sk,7), 16);
+        uint8_t MESG_all[1404];
+
+        for(int i=0;i<MESG_NUMBER_nCRC;i++)
+        {
+            MESG_all[i]=(uint8_t)ba.at(i);
+        }
+      //  qDebug()<<QString::number(SOC_CRC->FastCRC16(MESG_head,7), 16);
+        QByteArray check_crc16;
+        check_crc16.append(ba.at(8));
+        check_crc16.append(ba.at(7));
+        qDebug()<<check_crc16.toHex().toInt(&ok, 16);
+        qDebug()<<QString::number(SOC_CRC->FastCRC16(MESG_head,7), 10);
+        qDebug()<<QString::number(SOC_CRC->FastCRC32(MESG_all,MESG_NUMBER_nCRC), 16);
+
                   //  QString HEXNum = QString("%1").arg(setNum,0,16,QLatin1Char('0')).toUtf8();;
     }
 }
